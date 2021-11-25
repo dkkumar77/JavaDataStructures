aed9e8e4b13c7a10e7ab9c7f869fc7d323c8dd4c
Deepak Kumar <bcs.deepakkumar@gmail.com>
2021-11-25 14:26:17 -0500

    added Binary package

---
+++ src/BinaryTree/BinaryTree.java
@@ -0,0 +1,63 @@
+package BinaryTree;
+
+public class BinaryTree<T> {
+    private class Node<T>{
+
+        protected T data;
+        public Node<T> left, right;
+
+        public Node(T data){
+            this(data, null, null);
+
+        }
+        public Node(T data, Node<T> left, Node<T> right){
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+        public T getData(){
+            return data;
+        }
+
+        public Node<T> getLeft(){
+            return left;
+        }
+        public Node<T> getRight(){
+            return right;
+        }
+
+        public void setData(T data){
+            this.data = data;
+
+        }
+
+        public void setRight(Node<T> right){
+            this.right = right;
+        }
+
+        public void setLeft(Node<T> left){
+            this.left = left;
+        }
+    }
+
+
+    Node<T> root;
+
+
+    BinaryTree(){
+        this(null,null,null);
+    }
+
+    BinaryTree(T data){
+        this(data,null,null);
+
+    }
+
+    BinaryTree(T data, BinaryTree<T> left, BinaryTree<T> right){
+        this.root = new Node<T>(data);
+        root.setLeft(left.root);
+        root.setRight(left.root);
+
+    }
+
+}
